2d7415fc2258e7e8498c118152feaba51c18150c
Merik Westerveld <merik.westerveld@student.fontys.nl>
2019-04-18 17:45:36 +0200

    Fix ui for smaller phones

--- Paaspop/src/screens/PlacesScreen/PlacesScreen.js
+++ Paaspop/src/screens/PlacesScreen/PlacesScreen.js
@@ -114,7 +114,7 @@ PlacesScreen.defaultProps = {
 
 const styles = StyleSheet.create({
   container: {
-    height: '95%',
+    height: '90%',
     marginTop: '20%',
     marginLeft: '3%',
     marginRight: '5%',
